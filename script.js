dd13d64aae7efe0ec8130dec80ac1c5a132fdca7
salman <hashmisalman7040@gmail.com>
2024-01-28 23:27:47 +0530

    we work on it

---
+++ script.js
@@ -0,0 +1,10 @@
+// let myI = document.getElementsByClassName("myI");
+// let myList = document.getElementsByClassName("myList");
+
+// for (let i = 0; i < myI.length; i++) {
+//   myI[i].addEventListener("mouseover", () => {
+//     myList[i].style.visibility = "visible";
+//   });
+// }
+
+
