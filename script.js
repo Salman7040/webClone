f5e6f566047c7cc25d9127a4b27e82b15fad9cd4
arfatsawant <arfatsawant8743@gmail.com>
2024-01-29 20:47:49 +0530

    work has been left

--- script.js
+++ script.js
@@ -15,28 +15,28 @@ function hide(x) {
 
 let flag = true
 function searchOn() {
-  
+
   if (flag == true) {
     document.getElementsByClassName("search-bar")[0].style.visibility = "visible"
     flag = false
-  
+
   }
   else {
     document.getElementsByClassName("search-bar")[0].style.visibility = "hidden"
-    
+
     flag = true
   }
 }
 
 
-let flag1=true
-function hideChild(x){
+let flag1 = true
+function hideChild(x) {
   if (flag1 == true) {
-    x.children[1].style.visibility="visible"
-    flag1 = false  
+    x.children[1].style.visibility = "visible"
+    flag1 = false
   }
   else {
-    x.children[1].style.visibility="hidden"
+    x.children[1].style.visibility = "hidden"
     flag1 = true
   }
 }
@@ -44,11 +44,41 @@ function hideChild(x){
 
 
 //======= on slider change ===========
+var inc = 2;
+let slider = document.getElementsByClassName("slider")
+let slide = document.getElementsByClassName("main")
 
-function sliderChange(myThis,para){
+setInterval(function () {
 
-  myThis.style.background="#20366b"
-  document.getElementsByClassName("main")[0].style.background="url('./images/slide-"+para+".jpg')"
+  slide[0].style.background = "url('./images/slide-" + inc + ".jpg')"
+  slide[0].style.backgroundRepeat = "no-repeat"
+
+
+  slider[inc - 1].style.background = "#20366b"
+  for (let i = 0; i < slider.length; i++) {
+    if ((inc-1) != i)
+      slider[i].style.background = "white"
+  }
+
+  console.log(inc-1)
+  inc++;
+
+  if (inc >= 4)
+    inc = 1
+}, 3000);
+
+
+
+function sliderChange(myThis, para) {
+
+  slider[para].style.background = "#20366b"
+  for (let i = 0; i < slider.length; i++) {
+    if (para != i)
+      slider[i].style.background = "white"
+  }
+
+  slide[0].style.background = "url('./images/slide-" + (para + 1) + ".jpg')"
+  slide[0].style.backgroundRepeat = "no-repeat"
 
 }
 
